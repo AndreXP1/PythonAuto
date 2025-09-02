5355a6c355067458d289bacb7f551bfc4025831f
AndreXP1 <suporteandre02@gmail.com>
2025-09-02 10:29:06 -0300

    Removed more unused api

--- python/test_selenium.py
+++ python/test_selenium.py
@@ -1,9 +1,6 @@
 import os
 import random
 import time
-from urllib.parse import urljoin
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
