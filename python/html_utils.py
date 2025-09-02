b075786415d8fb2648794be6714afb3e0b489e01
AndreXP1 <suporteandre02@gmail.com>
2025-09-02 10:49:37 -0300

    Fix some conflitcs
    Changed the way the functions are imported, was causing the default.html
    file to not be created, and also the other way was missing some imports
    to work, and asking for parameters into the functions which wasn't
    working

--- python/html_utils.py
+++ python/html_utils.py
@@ -1,6 +1,54 @@
-#html_utils.py
-def wait_last_page(): ...
-def insert_body_into_html(): ...
+from bs4 import BeautifulSoup
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-#test_selenium.py
-from createHTML import wait_last_page as wp, insert_body_into_html as ib
+def wait_last_page(nav):
+    try:
+        nav.save_screenshot("prints/PageItem.png")
+        #Espera carregamento da pagina.
+        WebDriverWait(nav, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@class="page-product"]'))
+        )
+        #Captura body elements dentro da pagina
+        soup = BeautifulSoup(nav.page_source, 'html.parser')
+        body_tag = soup.body
+        #Retorna body em formato de string
+        if body_tag:
+            return str(body_tag)
+        else:
+            print("No body tag found")
+            return ""
+    except Exception as e:
+        print(f"Error during page loading: {e}")
+
+
+#Função que adiciona o body pego pelo scraping dentro de outro arquivo
+def insert_body_into_html(body_content, src_path="default.html", dst_path="tests/default.html"):
+
+#Cria diretório "/tests" se anida não existir
+    dst_dir = os.path.dirname(dst_path)
+    if dst_dir and not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+
+#Le arquivo original
+    with open(src_path, "r", encoding="utf-8") as f:
+        html = f.read()
+
+    soup = BeautifulSoup(html, "html.parser")
+
+#Se tiver tag body no arquivo orignal remove ela
+    if soup.body:
+        soup.body.decompose()
+
+#Cria nova tag body e insere o conteudo nela.
+    new_body = soup.new_tag("body")
+    new_body.append(BeautifulSoup(body_content, "html.parser"))
+    soup.html.append(new_body)
+
+#Passa o body para o outro arquivo
+    with open(dst_path, "w", encoding="utf-8") as f:
+        f.write(str(soup))
+
+    print(f"Body content inserted into {dst_path}")
